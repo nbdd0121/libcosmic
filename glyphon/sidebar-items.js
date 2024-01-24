2e9790780a75e9159b4a90ad767a6b5564bb0f78
mmstick <mmstick@users.noreply.github.com>
2024-01-24 17:47:19 +0000

    deploy: fe7ec072b97aefb295fd3320540c45facabed6fb

---
+++ glyphon/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ColorMode","PrepareError","RenderError"],"struct":["Resolution","TextArea","TextAtlas","TextBounds","TextRenderer"]};
---
+++ trait.impl/as_raw_xcb_connection/trait.AsRawXcbConnection.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"as_raw_xcb_connection":[],
+"tiny_xlib":[["impl <a class=\"trait\" href=\"as_raw_xcb_connection/trait.AsRawXcbConnection.html\" title=\"trait as_raw_xcb_connection::AsRawXcbConnection\">AsRawXcbConnection</a> for <a class=\"struct\" href=\"tiny_xlib/struct.Display.html\" title=\"struct tiny_xlib::Display\">Display</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
